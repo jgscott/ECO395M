3fe7b5facdd3e166d72e0ad9346e503bb92080f4
James Scott <james.scott@mccombs.utexas.edu>
2019-02-20 11:32:27 -0600

    train/test splits

--- r/saratoga_lm.R
+++ r/saratoga_lm.R
@@ -2,9 +2,10 @@ library(tidyverse)
 library(mosaic)
 data(SaratogaHouses)
 
+summary(SaratogaHouses)
+
 # Baseline model
-lm_small = lm(price ~ livingArea + lotSize + fireplaces, data=SaratogaHouses)
-simple_anova(lm_small)
+lm_small = lm(price ~ bedrooms + bathrooms + lotSize, data=SaratogaHouses)
 
 # 11 main effects
 lm_medium = lm(price ~ lotSize + age + livingArea + pctCollege + bedrooms + 
@@ -22,7 +23,6 @@ coef(lm_medium2)
 # All interactions
 # the ()^2 says "include all pairwise interactions"
 lm_big = lm(price ~ (. - sewer - waterfront - landValue - newConstruction)^2, data=SaratogaHouses)
-simple_anova(lm_big)
 
 
 ####
@@ -58,4 +58,26 @@ rmse(saratoga_test$price, yhat_test2)
 rmse(saratoga_test$price, yhat_test3)
 
 
-		
+# easy averaging over train/test splits
+library(mosaic)
+
+rmse_vals = do(100)*{
+  
+  # re-split into train and test cases
+  n_train = round(0.8*n)  # round to nearest integer
+  n_test = n - n_train
+  train_cases = sample.int(n, n_train, replace=FALSE)
+  test_cases = setdiff(1:n, train_cases)
+  saratoga_train = SaratogaHouses[train_cases,]
+  saratoga_test = SaratogaHouses[test_cases,]
+  
+  # fit to this training set
+  lm2 = lm(price ~ . - sewer - waterfront - landValue - newConstruction, data=saratoga_train)
+  
+  # predict on this testing set
+  yhat_test2 = predict(lm2, saratoga_test)
+  rmse(saratoga_test$price, yhat_test2)
+}
+
+rmse_vals
+colMeans(rmse_vals)
